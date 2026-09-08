5f47cbbd08d2f4a39e3e408fe8f6fda16dff7e11
ShiroHaku30 <thwwen@gmail.com>
2026-09-08 15:39:58 +0800

    [FEATURE]Improved commands and filter commands for users

---
+++ cmd/root.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ldap-extractor",
+	Short: "LDAP extraction tool",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
